d9849dbecabc4cb4bba504ea4cd6a3a9972d85dc
Yur3te <yur3te@gmail.com>
2026-01-04 06:48:30 +0100

    TranslateException class improvement, added Word getter

--- TranslateException.cs
+++ TranslateException.cs
@@ -2,10 +2,12 @@ namespace SlownikProjekt
 {
     public class TranslationException : Exception
     {
+        public string Word { get; }
+
         public TranslationException(string word)
             : base($"Nieznane słowo: {word}")
         {
-            
+            Word = word;
         }
     }
 
