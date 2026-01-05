c09b4ae0e13e6dbe005ea34733ec3501139ea982
Yur3te <yur3te@gmail.com>
2026-01-05 15:04:16 +0100

    Fixed a issue with TranslationRecord constructor and proper saving clinet info in history

--- TranslationRecord.cs
+++ TranslationRecord.cs
@@ -5,14 +5,15 @@ namespace SlownikProjekt
         public string OriginalText { get; set; }
         public string TranslatedText { get; set; }
         public DateTime Date { get; set; }
-        public Client Client { get; }
+        public string ClientName { get; set; }
 
+        public TranslationRecord() {}
 
         public TranslationRecord(Client client, string original, string translated)
         {
             OriginalText = original;
             TranslatedText = translated;
-            Client = client;
+            ClientName = client.Name;
             Date = DateTime.Now;
         }
    
