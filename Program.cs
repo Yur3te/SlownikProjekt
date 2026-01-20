9d307e6d141005a027d49cc9c2fdce1f00a0186b
Yur3te <yur3te@gmail.com>
2026-01-20 15:18:48 +0100

    Project goal comments cleanup

--- Program.cs
+++ Program.cs
@@ -1,20 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Projekt 6. 
-// Program automatycznie tłumaczący teksty z polskiego na angielski
-// funkcjonalność: program ma rozumieć tylko 8 (słownik) słów i tłumaczyć słowa 1:1, bez żadnej gramatyki, Zakładamy, że tylko te 8 słów wystąpi w tłumaczonym tekście. Zlecenia przychodzą od klientów.
-// 1. Zaprojektować odpowiednią strukturę klas - zastanowić się jakie klasy i jakie relacje między nimi będą potrzebne 
-//   (słowo, słownik, tekst, klient, inne ???)
-// 2. Zaimplementować w odpowiednich klasach metody umożliwiające tłumaczenie tekstów i wyszukiwanie informacji o już przetłumaczonych tekstach przy pomocy zapytań LINQ , w tym ile przetłumaczono tekstów zleconych przez każdego klienta oraz jaka była średnia długość tekstów zlecanych przez każdego z klientów.
-// 3. Odczytujemy teksty do tłumaczenia z plików tekstowych i do plików tekstowych zapisujemy przetłumaczone teksty.
-// 4. Za pomocą eventów wykonać powiadamianie klasy nadrzędnej o błędzie, jeśli w tekście będzie słowo spoza słownika.
-
-// klasa tłumaczienie, tekst oryginalne, przetłumaczony i data do historii
-// druga lista słów w słowniku pol ang - ang pol
-
-// TODO: wykres ile tłumaczeń w miesiącu
-
-
-using System;
+﻿using System;
 
 namespace SlownikProjekt
 {
